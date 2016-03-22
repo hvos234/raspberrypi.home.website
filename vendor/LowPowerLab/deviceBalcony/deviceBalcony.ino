865943dfa6cdeb2b215a0455325513699984e7a2
Vos <h.vos234@chello.nl>
2016-03-22 23:40:54 +0100

    Changed biterate and power consumption to increase range.

--- vendor/LowPowerLab/deviceBalcony/deviceBalcony.ino
+++ vendor/LowPowerLab/deviceBalcony/deviceBalcony.ino
@@ -39,6 +39,15 @@ void setup() {
   delay(10);
   radio.initialize(FREQUENCY,NODEID,NETWORKID);
   //radio.setHighPower(); //uncomment only for RFM69HW!
+  
+  // this change the bitrate from 4800 to 1200, and Frequenty level to max
+  radio.writeReg(0x03,0x68);      //RegBitrateMsb 1200 bitrate
+  radio.writeReg(0x04,0x2B);      //RegBitrateLsb 1200 bitrate
+  radio.writeReg(0x05,0x00);      //RegFdevMsb     2000 
+  radio.writeReg(0x06,0x52);      //RegFdevLsb     2000
+  radio.writeReg(0x19,0x40|0x10|0x05);      //RegRxBw  DccFreq:010, RxBw_Mant:24, RxBw_Exp:5 
+  radio.writeReg(0x18,0x00|0x00|0x01);      //RegLna  LnaZin:50ohm, LowPower:Off, CurrentGain:MAX
+  
   radio.encrypt(KEY);
   radio.promiscuous(promiscuousMode);
   
--- vendor/LowPowerLab/deviceMaster/deviceMaster.ino
+++ vendor/LowPowerLab/deviceMaster/deviceMaster.ino
@@ -9,7 +9,7 @@
 #define FREQUENCY   RF69_433MHZ //Match this with the version of your Moteino! (others: RF69_433MHZ, RF69_868MHZ)
 #define KEY         "sampleEncryptKey" //has to be same 16 characters/bytes on all nodes, not more not less!
 #define SERIAL_BAUD 9600
-#define TIMEOUT     3000 // wait for respones
+#define TIMEOUT     6000 // wait for respones
 
 boolean requestACK = false;
 RFM69 radio;
@@ -35,6 +35,10 @@ char message[17];
 int ac;
 char msg[15];
 
+boolean serial_read = false; // out of loop or the if true statement will read always false
+boolean serial_received = false;
+int serial_i = 0;
+
 //String serial_receive = "";
 char serial_receive[27]; // same as payload
 char serial_send[27];
@@ -50,6 +54,15 @@ void setup() {
   delay(10);
   radio.initialize(FREQUENCY,NODEID,NETWORKID);
   //radio.setHighPower(); //uncomment only for RFM69HW!
+  
+  // this change the bitrate from 4800 to 1200, and Frequenty level to max
+  radio.writeReg(0x03,0x68);      //RegBitrateMsb 1200 bitrate
+  radio.writeReg(0x04,0x2B);      //RegBitrateLsb 1200 bitrate
+  radio.writeReg(0x05,0x00);      //RegFdevMsb     2000 
+  radio.writeReg(0x06,0x52);      //RegFdevLsb     2000
+  radio.writeReg(0x19,0x40|0x10|0x05);      //RegRxBw  DccFreq:010, RxBw_Mant:24, RxBw_Exp:5 
+  radio.writeReg(0x18,0x00|0x00|0x01);      //RegLna  LnaZin:50ohm, LowPower:Off, CurrentGain:MAX
+  
   radio.encrypt(KEY);
   radio.promiscuous(promiscuousMode);
   
@@ -112,92 +125,125 @@ void loop() {
   //process any serial input
   if (Serial.available() > 0)
   {
+    
     memset(&serial_receive, 0, sizeof(serial_receive)); // clear char array
+    
     //serial_receive = "";
     //serial_receive = Serial.readBytesUntil('\0');
-    Serial.readBytesUntil(10, serial_receive, 32);
-    Serial.print("Serial Received: ");
-    Serial.println(serial_receive);
+    //Serial.readBytesUntil(10, serial_receive, 32);
     
-    serial_fr = 0;
-    serial_to = 0;
-    serial_ac = 0;
-    sscanf((char *)serial_receive, "fr:%d,to:%d,ac:%d", &serial_fr, &serial_to, &serial_ac);
-    
-    Serial.print("Serial From: ");
-    Serial.println(serial_fr);
-    Serial.print("Serial To: ");
-    Serial.println(serial_to);
-    Serial.print("Serial Action: ");
-    Serial.println(serial_ac);
-    
-    if(NODEID == serial_fr && NODEID == serial_to){ // if its for me
-      
-      memset(&serial_message, 0, sizeof(serial_message)); // clear it
-      if(ACTIONTEMP == serial_ac || ACTIONHUM == serial_ac || ACTIONTEMPHUM == serial_ac){
-        messageTempHum(ac, serial_message);
+    while (Serial.available () > 0) {
+      char serial_char = Serial.read ();
+      Serial.print("Serial While Char: ");
+      Serial.println(serial_char);
         
-        Serial.print("Serial Message: ");
-        Serial.println(serial_message);
+      if('^' == serial_char){
+        Serial.println("Serial Begin .. ");
+        serial_read = true;
+        serial_received = false;
+        serial_i = 0;
         
-      }else {
-        Serial.println("Action does not exist !");
-        sprintf(serial_message, "err:%s", "ac no");
+      }else if ('$' == serial_char){
+        Serial.println("Serial End .. ");
+        serial_receive[serial_i] = '\0';
+        
+        serial_read = false;
+        serial_received = true;
+        serial_i = 0;
+        
+      }else if(serial_read){
+        Serial.print("Serial Char: ");
+        Serial.println(serial_char);
+        serial_receive[serial_i] = serial_char;
+        serial_i++;
       }
+    }
+    
+    if(serial_received){
+      Serial.print("Serial Received: ");
+      Serial.println(serial_receive);
+      serial_received = false;
       
-    }else if(NODEID == serial_fr && 0 != serial_to && 0 != serial_ac){
+      serial_fr = 0;
+      serial_to = 0;
+      serial_ac = 0;
+      sscanf((char *)serial_receive, "fr:%d,to:%d,ac:%d", &serial_fr, &serial_to, &serial_ac);
       
-      memset(&serial_message, 0, sizeof(serial_message)); // clear it
-      memset(&payload_send, 0, sizeof(payload_send)); // clear it
-      sprintf(payload_send, "ac:%d,msg:%s", serial_ac, message);
+      Serial.print("Serial From: ");
+      Serial.println(serial_fr);
+      Serial.print("Serial To: ");
+      Serial.println(serial_to);
+      Serial.print("Serial Action: ");
+      Serial.println(serial_ac);
       
-      Serial.println("Sending ..");
-      Serial.print("Payload size: ");
-      Serial.println(sizeof(payload_send));
-      Serial.print("Payload: ");
-      Serial.println(payload_send);
-      radio.send(serial_to, (const void*)(&payload_send), sizeof(payload_send), false);
-      delay(25); // make sure payload is send
-      
-      // Wait here until we get a response, or timeout (250ms)
-      unsigned long started_waiting_at = millis();
-      bool timeout = false;
-      
-      // wait for respones
-      while( ! radio.receiveDone() && ! timeout ) {
-        if (millis() - started_waiting_at > TIMEOUT ) {
-          timeout = true;
-        }
-      }
-      
-      if ( timeout ) {
-        Serial.println("Nothing recieved !");
+      if(NODEID == serial_fr && NODEID == serial_to){ // if its for me
         
-      } else {
-        memset(&payload_receive, 0, sizeof(payload_receive)); // clear it
-        for (byte i = 0; i < radio.DATALEN; i++)
-        {
-          payload_receive[i] = (char)radio.DATA[i];
+        memset(&serial_message, 0, sizeof(serial_message)); // clear it
+        if(ACTIONTEMP == serial_ac || ACTIONHUM == serial_ac || ACTIONTEMPHUM == serial_ac){
+          messageTempHum(ac, serial_message);
+          
+          Serial.print("Serial Message: ");
+          Serial.println(serial_message);
+          
+        }else {
+          Serial.println("Action does not exist !");
+          sprintf(serial_message, "err:%s", "ac no");
         }
         
-        Serial.println("Received ..");
-        Serial.print("Sender id: ");
-        Serial.println(radio.SENDERID, DEC);
-        Serial.print("Sender RX RSSI: ");
-        Serial.println(radio.readRSSI());
+      }else if(NODEID == serial_fr && 0 != serial_to && 0 != serial_ac){
+        
+        memset(&serial_message, 0, sizeof(serial_message)); // clear it
+        memset(&payload_send, 0, sizeof(payload_send)); // clear it
+        sprintf(payload_send, "ac:%d,msg:%s", serial_ac, message);
+        
+        Serial.println("Sending ..");
         Serial.print("Payload size: ");
-        Serial.println(sizeof(payload_receive));
+        Serial.println(sizeof(payload_send));
         Serial.print("Payload: ");
-        Serial.println(payload_receive);
+        Serial.println(payload_send);
+        radio.send(serial_to, (const void*)(&payload_send), sizeof(payload_send), false);
+        delay(25); // make sure payload is send
         
-        ac = 0;
-        memset(&msg, 0, sizeof(msg)); // clear it
-        sscanf((char *)payload_receive, "ac:%d,msg:%s", &ac, &msg);
+        // Wait here until we get a response, or timeout (250ms)
+        unsigned long started_waiting_at = millis();
+        bool timeout = false;
         
-        Serial.print("Action: ");
-        Serial.println(ac);
-        Serial.print("Message: ");
-        Serial.println(msg);
+        // wait for respones
+        while( ! radio.receiveDone() && ! timeout ) {
+          if (millis() - started_waiting_at > TIMEOUT ) {
+            timeout = true;
+          }
+        }
+        
+        if ( timeout ) {
+          Serial.println("Nothing recieved !");
+          
+        } else {
+          memset(&payload_receive, 0, sizeof(payload_receive)); // clear it
+          for (byte i = 0; i < radio.DATALEN; i++)
+          {
+            payload_receive[i] = (char)radio.DATA[i];
+          }
+          
+          Serial.println("Received ..");
+          Serial.print("Sender id: ");
+          Serial.println(radio.SENDERID, DEC);
+          Serial.print("Sender RX RSSI: ");
+          Serial.println(radio.readRSSI());
+          Serial.print("Payload size: ");
+          Serial.println(sizeof(payload_receive));
+          Serial.print("Payload: ");
+          Serial.println(payload_receive);
+          
+          ac = 0;
+          memset(&msg, 0, sizeof(msg)); // clear it
+          sscanf((char *)payload_receive, "ac:%d,msg:%s", &ac, &msg);
+          
+          Serial.print("Action: ");
+          Serial.println(ac);
+          Serial.print("Message: ");
+          Serial.println(msg);
+        }
       }
     }
   }
